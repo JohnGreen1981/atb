9debd3213354fcd7f71911835c9afd38379ad748
JohnGreen1981 <6284624@gmail.com>
2025-05-27 20:45:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from openai import OpenAI
 from dotenv import load_dotenv
 
+load_dotenv()  # если ты локально тестируешь и используешь .env файл
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 app = FastAPI()
